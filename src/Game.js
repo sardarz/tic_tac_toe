e9cdb6b404557d23bf4217b3fdd32029e67a6137
sardarz <muhkween@gmail.com>
2022-09-20 12:18:08 +0300

    add game page header

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,25 @@
 import React from "react";
+import logo from "./assets/logo.svg"
+import restartIcon from "./assets/icon-restart.svg"
 
 const Game = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <main className="game">
+      <div className="container">
+        <div className="game-content">
+          <div className="game-header">
+            <div className="game-logo">
+              <img src={logo} alt="" />
+            </div>
+            <p className="game-turn"><span className="game-current-turn">X</span>turn</p>
+            <div className="game-restart">
+              <img src={restartIcon} alt="" />
+            </div> 
+          </div>
+
+        </div>
+      </div>
+    </main>
   );
 };
 
