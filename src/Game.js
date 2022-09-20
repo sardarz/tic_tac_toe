74c84d0a12704a1d237948b5aa7f45a9607c9f7f
sardarz <muhkween@gmail.com>
2022-09-20 19:35:30 +0300

    add winner checker functionality

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./assets/logo.svg";
 import restartIcon from "./assets/icon-restart.svg";
 import { ReactComponent as ICON_X_SMALL } from "./assets/icon-x-small.svg";
@@ -8,6 +8,69 @@ import { ReactComponent as ICON_X_OUTLINE } from "./assets/icon-x-outline.svg"
 import { ReactComponent as ICON_O_OUTLINE } from "./assets/icon-o-outline.svg"
 
 const Game = () => {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [boardState, setBoardState] = useState(new Array(9).fill(null));
+  const [score, setScore] = useState({x: 0, ties: 0, o: 0})
+  const boardRef = useRef(null)
+
+  const getWinner = (inputArray) => {
+    const array = [];
+    for (let i = 0; i < inputArray.length; i += 3) {
+      array.push(inputArray.slice(i, i + 3));
+    }
+    const answers = [];
+
+    for (let i = 0; i < array.length; i++) {
+      let row = [];
+      let column = [];
+      for (let j = 0; j < array.length; j++) {
+        row.push(array[i][j])
+        column.push(array[j][i])
+      }
+      answers.push(row)
+      answers.push(column)
+    }
+    let leftDiagonal = [];
+    let rightDiagonal = [];
+    for (let i = 0; i < array.length; i++) {
+      leftDiagonal.push(array[i][i])
+      rightDiagonal.push(array[i][2 - i])
+    }
+    answers.push(leftDiagonal)
+    answers.push(rightDiagonal)
+    
+    for (let answer of answers) {
+      let [x, y, z] = answer
+      if (x === y && x === z && x) return x
+    }
+    console.log(boardState)
+    return null
+  }
+
+  useEffect(() => {
+    const winner = getWinner(boardState);
+    if (winner === "X") {
+      setScore({...score, x: score.x + 1})
+      setBoardState(new Array(9).fill(null))
+      setCurrentPlayer('X')
+      boardRef.current.classList.remove('current-o')
+      boardRef.current.classList.add('current-x')
+    }
+    else if (winner === "O") {
+      setScore({...score, o: score.o + 1})
+      setBoardState(new Array(9).fill(null))
+      setCurrentPlayer('X')
+      boardRef.current.classList.remove('current-o')
+      boardRef.current.classList.add('current-x')
+    } else if (boardState.filter(x => x).length === 9) {
+      setScore({...score, ties: score.ties + 1})
+      setBoardState(new Array(9).fill(null))
+      setCurrentPlayer('X')
+      boardRef.current.classList.remove('current-o')
+      boardRef.current.classList.add('current-x')
+    }
+  })
+
 
   const checkCell = (evt) => {
     const currentCell = evt.target;
@@ -28,8 +91,7 @@ const Game = () => {
     setCurrentPlayer(newPlayer);
     setBoardState(newGameBoard);
   };
-  const [currentPlayer, setCurrentPlayer] = useState("X");
-  const [boardState, setBoardState] = useState(new Array(9).fill(null));
+
 
   return (
     <main className="game">
@@ -50,7 +112,7 @@ const Game = () => {
             </div>
           </div>
 
-          <div className="game-board current-x" onClick={checkCell}>
+          <div className="game-board current-x" ref={boardRef} onClick={checkCell}>
             {boardState.map((cell, idx) => {
               return (
                 <div key={idx} className={`game-cell ${cell === null ? "empty-cell" : ""}`}>
@@ -73,17 +135,17 @@ const Game = () => {
               <p>
                 x <span>(you)</span>
               </p>
-              <div className="game-stat-output">14</div>
+              <div className="game-stat-output">{score.x}</div>
             </div>
             <div className="game-stats-tie">
               <p>ties</p>
-              <div className="game-stat-output">32</div>
+              <div className="game-stat-output">{score.ties}</div>
             </div>
             <div className="game-stats-o">
               <p>
                 o <span>(cpu)</span>
               </p>
-              <div className="game-stat-output">19</div>
+              <div className="game-stat-output">{score.o}</div>
             </div>
           </div>
         </div>
