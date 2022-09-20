f7ebde17a922c77eeb14664f3db418054aa70dee
sardarz <muhkween@gmail.com>
2022-09-20 15:07:06 +0300

    add basic functionality

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./assets/logo.svg";
 import restartIcon from "./assets/icon-restart.svg";
 import { ReactComponent as ICON_X_SMALL } from "./assets/icon-x-small.svg";
 import { ReactComponent as ICON_X } from "./assets/icon-x.svg";
 import { ReactComponent as ICON_O } from "./assets/icon-o.svg";
+import { ReactComponent as ICON_X_OUTLINE } from "./assets/icon-x-outline.svg"
+import { ReactComponent as ICON_O_OUTLINE } from "./assets/icon-o-outline.svg"
 
 const Game = () => {
+
+  const checkCell = (evt) => {
+    const currentCell = evt.target;
+    const gameBoard = Array.from(evt.currentTarget.children);
+    const currentIdx = gameBoard.findIndex((el) => el === currentCell);
+    if (boardState[currentIdx] !== null) return;
+
+    if (currentPlayer === "X") {
+      evt.currentTarget.classList.remove('current-x')
+      evt.currentTarget.classList.add('current-o')
+    } else if (currentPlayer === "O") {
+      evt.currentTarget.classList.remove('current-o')
+      evt.currentTarget.classList.add('current-x')
+    }
+    let newGameBoard = boardState.slice();
+    newGameBoard[currentIdx] = currentPlayer;
+    let newPlayer = currentPlayer === "X" ? "O" : "X";
+    setCurrentPlayer(newPlayer);
+    setBoardState(newGameBoard);
+  };
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [boardState, setBoardState] = useState(new Array(9).fill(null));
+
   return (
     <main className="game">
       <div className="container">
@@ -20,25 +45,27 @@ const Game = () => {
               </div>
               <p>turn</p>
             </div>
-            <div className="game-restart">
+            <div className="game-restart" onClick={() => {setBoardState(new Array(9).fill(null))}}>
               <img src={restartIcon} alt="" />
             </div>
           </div>
 
-          <div className="game-board">
-            <div className="game-cell">
-              <ICON_X />
-            </div>
-            <div className="game-cell">
-              <ICON_O />
-            </div>
-            <div className="game-cell"></div>
-            <div className="game-cell"></div>
-            <div className="game-cell"></div>
-            <div className="game-cell"></div>
-            <div className="game-cell"></div>
-            <div className="game-cell"></div>
-            <div className="game-cell"></div>
+          <div className="game-board current-x" onClick={checkCell}>
+            {boardState.map((cell, idx) => {
+              return (
+                <div key={idx} className={`game-cell ${cell === null ? "empty-cell" : ""}`}>
+                  {boardState[idx] === "X" ? (
+                    <ICON_X />
+                  ) : boardState[idx] === "O" ? (
+                    <ICON_O />
+                  ) : (
+                    ""
+                  )}
+                  <ICON_O_OUTLINE />
+                  <ICON_X_OUTLINE />
+                </div>
+              );
+            })}
           </div>
 
           <div className="game-stats">
@@ -49,9 +76,7 @@ const Game = () => {
               <div className="game-stat-output">14</div>
             </div>
             <div className="game-stats-tie">
-              <p>
-                ties 
-              </p>
+              <p>ties</p>
               <div className="game-stat-output">32</div>
             </div>
             <div className="game-stats-o">
